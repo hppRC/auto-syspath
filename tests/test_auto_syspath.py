868bede3d6ef025d8176fdd4a0e114b93f83fbdd
hppRC <hpp.ricecake@gmail.com>
2021-07-07 23:37:12 +0900

    :sparkles: Add auto append for poetry

--- tests/test_auto_syspath.py
+++ tests/test_auto_syspath.py
@@ -1,5 +1,8 @@
+import sys
 import pytest
 from auto_syspath import __version__
+
+orig_path = sys.path[:]
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -18,6 +21,7 @@ def test_cwd():
         import fuga as _
     with pytest.raises(ModuleNotFoundError):
         import huga.fuga as _
+    sys.path = orig_path
 
 
 def test_src():
@@ -30,7 +34,20 @@ def test_src():
 
     with pytest.raises(ModuleNotFoundError):
         import fuga as _
+    sys.path = orig_path
 
+
+def test_poetry():
+    import auto_syspath.poetry as _
+
+    import src as _
+    import hoge as _
+    import huga as _
+    with pytest.raises(ModuleNotFoundError):
+        import fuga as _
+    import huga.fuga as _
+
+    sys.path = orig_path
 
 def test_all():
     import auto_syspath.all as _
@@ -40,3 +57,5 @@ def test_all():
     import huga as _
     import fuga as _
     import huga.fuga as _
+
+    sys.path = orig_path
